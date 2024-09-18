0797a98e32c487231e31215ac7b0cc4da727693c
Smexo <larushkinevgeniy.ru@gmail.com>
2024-09-18 18:37:36 +0500

    Ларюшкин Евгений Задание 8

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -13,8 +13,16 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        long multipli = 1;
+        if(arr.length > 0){
+            for(int i = 0; i < arr.length; i++){
+                multipli *= arr[i];
+            }
+            return multipli;
+        }
+        else {
+            return 0;
+        }
     }
 
 }
